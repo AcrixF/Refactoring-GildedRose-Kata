9701d7b823325497f894fe3f6a4a31bcf4329426
Cristian <acrixf@gmail.com>
2020-12-19 20:47:16 -0600

    extract Item update logic

--- src/test/java/org/neoa/gildedrose/GildedRoseTest.java
+++ src/test/java/org/neoa/gildedrose/GildedRoseTest.java
@@ -10,10 +10,14 @@ public class GildedRoseTest {
         String name = "foo";
         int sellIn = 0;
         int quality = 0;
+        String response = updateItem(name, sellIn, quality);
+        Approvals.verify(response);
+    }
+
+    private String updateItem(String name, int sellIn, int quality) {
         Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        String response = app.items[0].toString();
-        Approvals.verify(response);
+        return app.items[0].toString();
     }
 }
