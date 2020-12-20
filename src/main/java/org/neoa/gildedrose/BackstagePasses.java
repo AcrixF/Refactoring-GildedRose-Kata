7e4a408263a783fd196dde926eab7fc4a3560ee9
Cristian <acrixf@gmail.com>
2020-12-20 13:26:03 -0600

    create subclasses item

---
+++ src/main/java/org/neoa/gildedrose/BackstagePasses.java
@@ -0,0 +1,7 @@
+package org.neoa.gildedrose;
+
+public class BackstagePasses extends Item {
+    public BackstagePasses(int sellIn, int quality) {
+        super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+}
