c880abc93f63fbc5a4d9765c8fbb4a9c52ac0120
Cristian <acrixf@gmail.com>
2020-12-19 22:13:42 -0600

    cover coverage(branch approach)

--- src/test/java/org/neoa/gildedrose/GildedRoseTest.java
+++ src/test/java/org/neoa/gildedrose/GildedRoseTest.java
@@ -8,9 +8,9 @@ public class GildedRoseTest {
     @Test
     void foo() {
         CombinationApprovals.verifyAllCombinations(this::updateItem,
-                new String []{"foo"},
-                new Integer []{0},
-                new Integer []{0});
+                new String []{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer []{-1, 0, 11},
+                new Integer []{0, 5, 49, 50});
     }
 
     private String updateItem(String name, int sellIn, int quality) {
